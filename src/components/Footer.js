0419de8eb99ec859b4ae11e6b9e5f505a3f31745
Kevin Edmondson <kevinraye13@gmail.com>
2019-04-14 00:08:21 -0500

    icons/footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-    <p className="copyright">
-      Built by: Kevin Edmondson. Built using:{' '}
-      <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
-    </p>
+    <p className="copyright">Built by: Kevin Edmondson</p>
   </footer>
 )
 
