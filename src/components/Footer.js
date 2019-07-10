16dc70e2e17e119c43468574905d4c91b73741a6
Kevin Edmondson <kevinraye13@gmail.com>
2019-07-10 10:57:55 -0500

    header changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-    <p className="copyright">Built by: Kevin Edmondson</p>
+    <p className="copyright" />
   </footer>
 )
 
